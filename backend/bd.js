face49661828b7ad847e4aea792c3fdd79b4dbad
melissa <melissahassani00@gmail.com>
2025-03-25 13:19:29 +0100

    description

--- backend/bd.js
+++ backend/bd.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const mysql =require("mysql2");
 
-const bd = mysql.createConnection({
-    host: "localhost",
-    user: " melissa",
-    password:"m.HN010423@",
-    database:""
-});
-
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+//connexion a mySQL
 bd.connect(err => {
     if(err){
         console.error("erreur de connexion a MySQL: ",err);
