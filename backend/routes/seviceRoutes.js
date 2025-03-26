adf1bcd939b6a1ea770a7d05c87d9e9ea639e121
melissa <melissahassani00@gmail.com>
2025-03-26 21:54:21 +0100

    description

---
+++ backend/routes/seviceRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const bd = require("../bd");
+const authMiddleware = require("../middleware/auth");
+
+//Ajouter une mission
+router.post("/add", authMiddleware, (req,res)=> {
+    const { titre, description, dureEstime, budget, statut } =req.body;
+    const idFreelancer = req.user.id;
+    
+    if(!titre || !description || !dureEstme || !budget ){
+        return res.status(400).json({ message: "⚠️ Tous les champs sont requis" });
+    }
+    const query = "INSERT INTO Service (titre, descirption, dureEstime, budget, statut, idFreelancer) VALUES(?,?,?,?)";
+
+    bd.query(query, [titre, description, dureEstime,budget,idFreelancer],(err)=>{
+        if(err) {
+            console.error("❌ Erreur lors de l'ajout de la mission :", err);
+            return res.status(500).json({ message: "Erreur serveur" });
+                }
+                res.status(201).json({ message: "✅ Mission ajoutée avec succès !" });
+            });
+        });
+        
+        module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,8 @@ const missionRoutes = require("./routes/missionRoutes");
 app.use("/missions", missionRoutes);
 const freelancerRoutes = require("./routes/freelanceurRoutes");
 app.use("/freelancers", freelancerRoutes);
+const serviceRoutes = require("./routes/freelanceurRoutes");
+app.use("/services",serviceRoutes);
 
 // 🌍 Route d'accueil
 app.get("/", (req, res) => {
